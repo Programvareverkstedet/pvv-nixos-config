1429a1b51bb2118221dde41367e12b565450c5c7
Felix Albrigtsen <felixalb@pvv.ntnu.no>
2022-12-17 22:00:57 +0100

    Simplify nginx on ildkule

--- hosts/ildkule/services/nginx/default.nix
+++ hosts/ildkule/services/nginx/default.nix
@@ -4,13 +4,11 @@
 
   security.acme = {
     acceptTerms = true;
-    defaults.email = "danio@pvv.ntnu.no";
+    defaults.email = "drift@pvv.ntnu.no";
   };
 
   services.nginx = {
     enable = true;
-
-    defaultListenAddresses = [ "129.241.210.187" "127.0.0.1" "127.0.0.2" "[2001:700:300:1900::187]" "[::1]" ];
 
     recommendedProxySettings = true;
     recommendedTlsSettings = true;
