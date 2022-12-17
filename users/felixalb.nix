50133047e67e574273ffd592287eb4d9866a16b3
Felix Albrigtsen <felixalb@pvv.ntnu.no>
2022-12-17 22:56:12 +0100

    Add felixalb user keys

--- users/felixalb.nix
+++ users/felixalb.nix
@@ -4,5 +4,8 @@
     isNormalUser = true;
     extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
     shell = pkgs.zsh;
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDKzPICGew7uN0cmvRmbwkwTCodTBUgEhkoftQnZuO4Q felixalbrigtsen@gmail.com"
+    ];
   };
 }
