40c67c6153113e953f465d7926426ded77c77adb
Felix Albrigtsen <felixalb@pvv.ntnu.no>
2022-12-20 23:15:52 +0100

    Removed motd extras

--- hosts/ildkule/configuration.nix
+++ hosts/ildkule/configuration.nix
@@ -3,11 +3,8 @@
   imports = [
       # Include the results of the hardware scan.
       ./hardware-configuration.nix
-
       ../../base.nix
-      ../../misc/rust-motd.nix
       ../../misc/metrics-exporters.nix
-      # Users can just import any configuration they want even for non-user things. Improve the users/default.nix to just load some specific attributes if this isn't wanted
 
       ./services/nginx
       ./services/metrics
