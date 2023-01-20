1524b6b10c7bd233f8435407b4fb575c9b91d8db
h7x4 <h7x4@nani.wtf>
2023-01-20 01:08:09 +0100

    metrics: stop parsing prometheus labels from url

--- hosts/jokum/services/matrix/synapse.nix
+++ hosts/jokum/services/matrix/synapse.nix
@@ -206,17 +206,22 @@ in {
     };
 
     locations."/metrics/" = let
-      endpoints = lib.pipe cfg.workers.instances [
+      # See https://github.com/matrix-org/synapse/blob/develop/docs/metrics-howto.md
+      staticConfigs = lib.pipe cfg.workers.instances [
         (lib.mapAttrsToList (_: v: v))
-        (map (w: "${w.type}/${toString w.index}"))
-        (map (w: "matrix.pvv.ntnu.no/metrics/${w}"))
-      ] ++ [ "matrix.pvv.ntnu.no/metrics/master/1" ];
+        # Add metrics for main process to the list of workers
+        (x: x ++ [{ type = "master"; index = 1; }])
+        (map (w: {
+          targets = [ "matrix.pvv.ntnu.no/metrics/${w.type}/${toString w.index}" ];
+          labels = {
+            instance = "matrix.pvv.ntnu.no";
+            job = w.type;
+            index = toString w.index;
+          };
+        }))
+      ];
     in {
-      alias = pkgs.writeTextDir "/config.json"
-        (builtins.toJSON [
-          { targets = endpoints;
-            labels = { };
-          }]) + "/";
+      alias = (pkgs.writeTextDir "/config.json" (builtins.toJSON staticConfigs)) + "/";
     };
   })];
 }
