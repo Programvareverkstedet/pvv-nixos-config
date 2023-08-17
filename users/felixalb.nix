6d6987c87ac4080b88bf8363806bfc7ceb89eaa4
Felix Albrigtsen <felix@albrigtsen.it>
2023-08-17 22:07:00 +0200

    felixalb: Add SSH key

--- users/felixalb.nix
+++ users/felixalb.nix
@@ -6,6 +6,7 @@
     shell = pkgs.zsh;
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDKzPICGew7uN0cmvRmbwkwTCodTBUgEhkoftQnZuO4Q felixalbrigtsen@gmail.com"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBTXSL0w7OUcz1LzEt1T3I3K5RgyNV+MYz0x/1RbpDHQ felixalb@worf"
     ];
   };
 }
