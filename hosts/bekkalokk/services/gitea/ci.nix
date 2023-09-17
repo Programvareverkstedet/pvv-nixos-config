5c529a023321973587d1184ab793d11dcbb07db8
h7x4 <h7x4@nani.wtf>
2023-09-17 04:05:08 +0200

    Fix gitea runners, add 2 more
    The gitea runners are now activated correctly,
    has support for both debian and ubuntu based systems,
    and can will connect to the gitea server through the
    loopback interface

--- hosts/bekkalokk/services/gitea/ci.nix
+++ hosts/bekkalokk/services/gitea/ci.nix
@@ -1,16 +1,30 @@
-{ config, ... }:
-{
-  sops.secrets."gitea/runner-token" = { };
+{ config, lib, values, ... }:
+let
+  mkRunner = name: {
+    # This is unfortunately state, and has to be generated one at a time :(
+    # To do that, comment out all except one of the runners, fill in its token
+    # inside the sops file, rebuild the system, and only after this runner has
+    # successfully registered will gitea give you the next token.
+    # - oysteikt Sep 2023
+    sops.secrets."gitea/runners/${name}".restartUnits = [
+      "gitea-runner-${name}.service"
+    ];
 
-  services.gitea-actions-runner.instances = {
-    runner1 = {
-      url = "https://git-runner1.pvv.ntnu.no";
-      name = "git-runner1";
-      labels = [
-        "debian-latest:docker://node:18-bullseye"
-      ];
-      enable = true;
-			tokenFile = config.sops.secrets."gitea/runner-token".path;
+    services.gitea-actions-runner.instances = {
+      ${name} = {
+        enable = true;
+        name = "git-runner-${name}"; url = "https://git.pvv.ntnu.no";
+        labels = [
+	  "debian-latest:docker://node:18-bullseye"
+	  "ubuntu-latest:docker://node:18-bullseye"
+	];
+        tokenFile = config.sops.secrets."gitea/runners/${name}".path;
+      };
     };
   };
-}
+in
+lib.mkMerge [
+  (mkRunner "alpha")
+  (mkRunner "beta")
+  (mkRunner "epsilon")
+]
