dd8b677a793e8b35e2b27ac2803881d8ff69260b
Daniel Olsen <daniel.olsen99@gmail.com>
2023-12-10 05:27:58 +0100

    buskerud: bootloader - 3.3TB, OS - 256GB 👍

--- hosts/buskerud/configuration.nix
+++ hosts/buskerud/configuration.nix
@@ -13,7 +13,7 @@
   # boot.loader.systemd-boot.enable = true;
   # boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.grub.enable = true;
-  boot.loader.grub.device = "/dev/sda";
+  boot.loader.grub.device = "/dev/sdb";
 
   networking.hostName = "buskerud";
   networking.search = [ "pvv.ntnu.no" "pvv.org" ];
