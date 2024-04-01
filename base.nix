fde69ca283e3c0d10b4eea8014bd0ef3366a4647
h7x4 <h7x4@nani.wtf>
2024-04-01 23:45:29 +0200

    base: add sops keys for everyone and everything

--- base.nix
+++ base.nix
@@ -84,6 +84,12 @@
     settings.PermitRootLogin = "yes";
   };
 
+  sops.age = {
+    sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
+    keyFile = "/var/lib/sops-nix/key.txt";
+    generateKey = true;
+  };
+
   # nginx return 444 for all nonexistent virtualhosts
 
   systemd.services.nginx.after = [ "generate-snakeoil-certs.service" ];
