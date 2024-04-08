d8e13ff67c6acd0f0e2bc49db938986e59973952
Adrian Gunnar Lauterer <adriangl@pvv.ntnu.no>
2024-04-08 21:21:24 +0200

    Update users/adriangl.nix

--- users/adriangl.nix
+++ users/adriangl.nix
@@ -6,15 +6,20 @@
     extraGroups = [
       "wheel"
       "drift"
+      "nix-builder-users"
     ];
 
     packages = with pkgs; [
-      eza
       neovim
+      htop
+      ripgrep
+      vim
     ];
 
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFa5y7KyLn2tjxed1czMbyM5scnEpo9v/GfnhL/28ckM legolas"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICf7SlyHR6KgP7+IeFr/Iuiu2lL5vaSlzqPonaO8XU0J gunalx@aragon"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEj+Y0RUrSaF8gUW8m2BY6i8e7/0bUWhu8u8KW+AoHDh gunalx@nixos"
     ];
   };
 }
