1591fdb7cc380470fd4b86c9c9ffec6834e82327
Øystein Tveit <oysteikt@pvv.ntnu.no>
2024-07-08 19:56:43 +0200

    bekkalokk/mediawiki: re-enable DeleteBatch extension

--- hosts/bekkalokk/services/mediawiki/default.nix
+++ hosts/bekkalokk/services/mediawiki/default.nix
@@ -86,8 +86,7 @@ in {
     };
 
     extensions = {
-      #inherit (pkgs.mediawiki-extensions) DeleteBatch UserMerge PluggableAuth SimpleSAMLphp VisualEditor;
-      inherit (pkgs.mediawiki-extensions) UserMerge PluggableAuth SimpleSAMLphp VisualEditor;
+      inherit (pkgs.mediawiki-extensions) DeleteBatch UserMerge PluggableAuth SimpleSAMLphp VisualEditor;
     };
 
     extraConfig = ''
