2ff69dfec66e031e09f76dc9ed98434ee4fa919f
Peder Bergebakken Sundt <pbsds@hotmail.com>
2024-08-14 17:25:55 +0200

    justfile: add recipe `run-vm`

--- base.nix
+++ base.nix
@@ -130,4 +130,12 @@
     acceptTerms = true;
     defaults.email = "drift@pvv.ntnu.no";
   };
+  # Let's not spam LetsEncrypt in `nixos-rebuild build-vm` mode:
+  virtualisation.vmVariant = {
+    security.acme.defaults.server = "https://127.0.0.1";
+    security.acme.preliminarySelfsigned = true;
+
+    users.users.root.initialPassword = "root";
+  };
+
 }
