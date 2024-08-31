69f98933a48ec94b167d593334c7aa621b0bcc7c
h7x4 <h7x4@nani.wtf>
2024-09-01 01:51:34 +0200

    common/smartd: add `smartctl` to environment packages

--- base/services/smartd.nix
+++ base/services/smartd.nix
@@ -1,4 +1,8 @@
-{ ... }:
+{ config, pkgs, lib, ... }:
 {
-  services.smartd.enable = true;
+  services.smartd.enable = lib.mkDefault true;
+
+  environment.systemPackages = lib.optionals config.services.smartd.enable (with pkgs; [
+    smartmontools
+  ]);
 }
