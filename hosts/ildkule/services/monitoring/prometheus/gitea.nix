c64d28fc2623aef78b9e07c2cab03cf0ef872636
Felix Albrigtsen <felixalb@pvv.ntnu.no>
2024-09-14 20:21:12 +0200

    Prometheus: Monitor gitea metrics

---
+++ hosts/ildkule/services/monitoring/prometheus/gitea.nix
@@ -0,0 +1,16 @@
+{ ... }:
+{
+  services.prometheus.scrapeConfigs = [{
+    job_name = "gitea";
+    scrape_interval = "60s";
+    scheme = "https";
+
+    static_configs = [
+      {
+        targets = [
+          "git.pvv.ntnu.no:443"
+        ];
+      }
+    ];
+  }];
+}
