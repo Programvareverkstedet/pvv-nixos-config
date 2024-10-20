f6e530692bc6e75e00b6a8287821cd14100329f5
Felix Albrigtsen <felix@albrigtsen.it>
2024-10-20 18:20:09 +0200

    Bakke: more disk stuff

--- hosts/bakke/configuration.nix
+++ hosts/bakke/configuration.nix
@@ -12,10 +12,17 @@
   sops.age.keyFile = "/var/lib/sops-nix/key.txt";
   sops.age.generateKey = true;
 
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader = {
+    efi.canTouchEfiVariables = true;
+    grub = {
+      enable = true;
+      efiSupport = true;
+      device = "nodev";
+   };
+  };
 
   networking.hostName = "bakke";
+  networking.hostId = "99609ffc";
   systemd.network.networks."30-enp2s0" = values.defaultNetworkConfig // {
     matchConfig.Name = "enp2s0";
     address = with values.hosts.bakke; [ (ipv4 + "/25") (ipv6 + "/64") ];
