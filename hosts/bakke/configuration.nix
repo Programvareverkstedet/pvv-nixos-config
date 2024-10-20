c9dec343dc7b0bac2cb8a6b58e1a669e07334515
Felix Albrigtsen <felix@albrigtsen.it>
2024-10-20 22:04:11 +0200

    bakke: get rid of disko 💀

--- hosts/bakke/configuration.nix
+++ hosts/bakke/configuration.nix
@@ -12,14 +12,8 @@
   sops.age.keyFile = "/var/lib/sops-nix/key.txt";
   sops.age.generateKey = true;
 
-  boot.loader = {
-    efi.canTouchEfiVariables = true;
-    grub = {
-      enable = true;
-      efiSupport = true;
-      device = "nodev";
-   };
-  };
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "bakke";
   networking.hostId = "99609ffc";
