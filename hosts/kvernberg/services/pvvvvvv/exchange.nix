bf071c64ff428c17c860b3c57074abd596228872
Daniel Olsen <daniel.olsen99@gmail.com>
2024-11-17 10:02:32 +0100

    kvernberg/taler/exchange: Add more denominations

--- hosts/kvernberg/services/pvvvvvv/exchange.nix
+++ hosts/kvernberg/services/pvvvvvv/exchange.nix
@@ -13,8 +13,122 @@ in {
     debug = true;
     openFirewall = true;
     denominationConfig = ''
-      [COIN-${CURRENCY}-k1-1-0]
+      ## Old denomination names cannot be used again
+      #[COIN-${CURRENCY}-k1-1-0]
+
+      ## NOK Denominations
+      [coin-${CURRENCY}-nok-1-0]
       VALUE = ${CURRENCY}:1
+      DURATION_WITHDRAW = 7 days
+      DURATION_SPEND = 1 years
+      DURATION_LEGAL = 3 years
+      FEE_WITHDRAW = ${CURRENCY}:0
+      FEE_DEPOSIT = ${CURRENCY}:0
+      FEE_REFRESH = ${CURRENCY}:0
+      FEE_REFUND = ${CURRENCY}:0
+      RSA_KEYSIZE = 2048
+      CIPHER = RSA
+      
+      [coin-${CURRENCY}-nok-5-0]
+      VALUE = ${CURRENCY}:5
+      DURATION_WITHDRAW = 7 days
+      DURATION_SPEND = 1 years
+      DURATION_LEGAL = 3 years
+      FEE_WITHDRAW = ${CURRENCY}:0
+      FEE_DEPOSIT = ${CURRENCY}:0
+      FEE_REFRESH = ${CURRENCY}:0
+      FEE_REFUND = ${CURRENCY}:0
+      RSA_KEYSIZE = 2048
+      CIPHER = RSA
+
+      [coin-${CURRENCY}-nok-10-0]
+      VALUE = ${CURRENCY}:10
+      DURATION_WITHDRAW = 7 days
+      DURATION_SPEND = 1 years
+      DURATION_LEGAL = 3 years
+      FEE_WITHDRAW = ${CURRENCY}:0
+      FEE_DEPOSIT = ${CURRENCY}:0
+      FEE_REFRESH = ${CURRENCY}:0
+      FEE_REFUND = ${CURRENCY}:0
+      RSA_KEYSIZE = 2048
+      CIPHER = RSA
+      
+      [coin-${CURRENCY}-nok-20-0]
+      VALUE = ${CURRENCY}:20
+      DURATION_WITHDRAW = 7 days
+      DURATION_SPEND = 1 years
+      DURATION_LEGAL = 3 years
+      FEE_WITHDRAW = ${CURRENCY}:0
+      FEE_DEPOSIT = ${CURRENCY}:0
+      FEE_REFRESH = ${CURRENCY}:0
+      FEE_REFUND = ${CURRENCY}:0
+      RSA_KEYSIZE = 2048
+      CIPHER = RSA
+      
+      [coin-${CURRENCY}-nok-50-0]
+      VALUE = ${CURRENCY}:50
+      DURATION_WITHDRAW = 7 days
+      DURATION_SPEND = 1 years
+      DURATION_LEGAL = 3 years
+      FEE_WITHDRAW = ${CURRENCY}:0
+      FEE_DEPOSIT = ${CURRENCY}:0
+      FEE_REFRESH = ${CURRENCY}:0
+      FEE_REFUND = ${CURRENCY}:0
+      RSA_KEYSIZE = 2048
+      CIPHER = RSA
+      
+      [coin-${CURRENCY}-nok-100-0]
+      VALUE = ${CURRENCY}:100
+      DURATION_WITHDRAW = 7 days
+      DURATION_SPEND = 1 years
+      DURATION_LEGAL = 3 years
+      FEE_WITHDRAW = ${CURRENCY}:0
+      FEE_DEPOSIT = ${CURRENCY}:0
+      FEE_REFRESH = ${CURRENCY}:0
+      FEE_REFUND = ${CURRENCY}:0
+      RSA_KEYSIZE = 2048
+      CIPHER = RSA
+      
+      [coin-${CURRENCY}-nok-200-0]
+      VALUE = ${CURRENCY}:200
+      DURATION_WITHDRAW = 7 days
+      DURATION_SPEND = 1 years
+      DURATION_LEGAL = 3 years
+      FEE_WITHDRAW = ${CURRENCY}:0
+      FEE_DEPOSIT = ${CURRENCY}:0
+      FEE_REFRESH = ${CURRENCY}:0
+      FEE_REFUND = ${CURRENCY}:0
+      RSA_KEYSIZE = 2048
+      CIPHER = RSA
+      
+      [coin-${CURRENCY}-nok-500-0]
+      VALUE = ${CURRENCY}:500
+      DURATION_WITHDRAW = 7 days
+      DURATION_SPEND = 1 years
+      DURATION_LEGAL = 3 years
+      FEE_WITHDRAW = ${CURRENCY}:0
+      FEE_DEPOSIT = ${CURRENCY}:0
+      FEE_REFRESH = ${CURRENCY}:0
+      FEE_REFUND = ${CURRENCY}:0
+      RSA_KEYSIZE = 2048
+      CIPHER = RSA
+      
+      [coin-${CURRENCY}-nok-1000-0]
+      VALUE = ${CURRENCY}:1000
+      DURATION_WITHDRAW = 7 days
+      DURATION_SPEND = 1 years
+      DURATION_LEGAL = 3 years
+      FEE_WITHDRAW = ${CURRENCY}:0
+      FEE_DEPOSIT = ${CURRENCY}:0
+      FEE_REFRESH = ${CURRENCY}:0
+      FEE_REFUND = ${CURRENCY}:0
+      RSA_KEYSIZE = 2048
+      CIPHER = RSA
+
+      ## PVV Special Prices
+      # 2024 pizza egenandel
+      [coin-${CURRENCY}-pvv-64-0]
+      VALUE = ${CURRENCY}:64
       DURATION_WITHDRAW = 7 days
       DURATION_SPEND = 1 years
       DURATION_LEGAL = 3 years
