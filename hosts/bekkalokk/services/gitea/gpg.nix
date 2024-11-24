cbec247a871604b5bc1ebb092e03f7d4ac293fc7
Øystein Tveit <oysteikt@pvv.ntnu.no>
2024-11-24 03:21:26 +0100

    rebase

--- hosts/bekkalokk/services/gitea/gpg.nix
+++ hosts/bekkalokk/services/gitea/gpg.nix
@@ -11,6 +11,11 @@ in
 
   systemd.services.gitea.environment = { inherit GNUPGHOME; };
 
+  systemd.tmpfiles.settings."20-gitea-gnugpg".${GNUPGHOME}.d = {
+    inherit (cfg) user group;
+    mode = "700";
+  };
+
   systemd.services.gitea-ensure-gnupg-homedir = {
     description = "Import gpg key for gitea";
     environment = { inherit GNUPGHOME; };
@@ -22,5 +27,12 @@ in
     script = ''
       ${lib.getExe pkgs.gnupg} --import ${config.sops.secrets."gitea/gpg-signing-key".path}
     '';
+  };
+
+  services.gitea.settings."repository.signing" = {
+    SIGNING_KEY = "0549C43374D2253C";
+    SIGNING_NAME = "PVV Git";
+    SIGNING_EMAIL = "gitea@git.pvv.ntnu.no";
+    INITIAL_COMMIT = "always";
   };
 }
