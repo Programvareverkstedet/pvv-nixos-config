86e68f496edce66041fcf274db5bd0da1a33ba95
h7x4 <h7x4@nani.wtf>
2025-01-10 18:51:40 +0100

    bekkalokk/gitea: add declarative label set 'projects'

--- hosts/bekkalokk/services/gitea/default.nix
+++ hosts/bekkalokk/services/gitea/default.nix
@@ -180,11 +180,16 @@ in {
         <a class="item" href="https://wiki.pvv.ntnu.no/">Wiki</a>
         <a class="item" href="https://git.pvv.ntnu.no/Drift/-/projects/4">Tokyo Drift Issues</a>
       '';
+
+      project-labels = (pkgs.formats.yaml { }).generate "gitea-project-labels.yaml" {
+        labels = lib.importJSON ./labels/projects.json;
+      };
     in ''
       install -Dm444 ${logo-svg} ${cfg.customDir}/public/assets/img/logo.svg
       install -Dm444 ${logo-png} ${cfg.customDir}/public/assets/img/logo.png
       install -Dm444 ${./loading.apng} ${cfg.customDir}/public/assets/img/loading.png
       install -Dm444 ${extraLinks} ${cfg.customDir}/templates/custom/extra_links.tmpl
+      install -Dm444 ${project-labels} ${cfg.customDir}/options/label/project-labels.yaml
     '';
   };
 }
