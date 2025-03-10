fc64139739cd518cf5d9da8fb805971d02884f6c
h7x4 <h7x4@nani.wtf>
2025-03-10 19:58:53 +0100

    bekkalokk/gitea: bigger icons

--- hosts/bekkalokk/services/gitea/customization.nix
+++ hosts/bekkalokk/services/gitea/customization.nix
@@ -28,12 +28,25 @@ in
       project-labels = (pkgs.formats.yaml { }).generate "gitea-project-labels.yaml" {
         labels = lib.importJSON ./labels/projects.json;
       };
+
+      customTemplates = pkgs.runCommandLocal "gitea-templates" {
+        nativeBuildInputs = with pkgs; [
+          coreutils
+          gnused
+        ];
+      } ''
+        # Bigger icons
+        install -Dm444 "${cfg.package.src}/templates/repo/icon.tmpl" "$out/repo/icon.tmpl"
+        sed -i -e 's/24/48/g' "$out/repo/icon.tmpl"
+      '';
     in ''
       install -Dm444 ${logo-svg} ${cfg.customDir}/public/assets/img/logo.svg
       install -Dm444 ${logo-png} ${cfg.customDir}/public/assets/img/logo.png
       install -Dm444 ${./loading.apng} ${cfg.customDir}/public/assets/img/loading.png
       install -Dm444 ${extraLinks} ${cfg.customDir}/templates/custom/extra_links.tmpl
       install -Dm444 ${project-labels} ${cfg.customDir}/options/label/project-labels.yaml
+
+      "${lib.getExe pkgs.rsync}" -a "${customTemplates}/" ${cfg.customDir}/templates/
     '';
   };
 }
