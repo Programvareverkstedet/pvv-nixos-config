e9c82b46253c9388a99379ff8a6ab897907cec19
h7x4 <h7x4@nani.wtf>
2025-03-15 14:33:13 +0100

    kommode/gitea: add robots.txt

--- flake.nix
+++ flake.nix
@@ -196,6 +196,7 @@
         modules = [
           inputs.nix-gitea-themes.nixosModules.default
           inputs.disko.nixosModules.disko
+          self.nixosModules.robots-txt
         ];
       };
 
