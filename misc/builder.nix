ace351c0a7dbfe440102dbd54c51c08a938cc286
h7x4 <h7x4@nani.wtf>
2025-03-15 01:23:15 +0100

    misc/builder: add binfmt systems

--- misc/builder.nix
+++ misc/builder.nix
@@ -3,4 +3,9 @@
 {
   nix.settings.trusted-users = [ "@nix-builder-users" ];
   nix.daemonCPUSchedPolicy = "batch";
+
+  boot.binfmt.emulatedSystems = [
+    "aarch64-linux"
+    "armv7l-linux"
+  ];
 }
