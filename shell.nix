069da368955ecfd1aca9c1129373a4fe7d5f188c
h7x4 <h7x4@nani.wtf>
2025-03-15 03:09:26 +0100

    shell.nix: replace shellHook with env

--- shell.nix
+++ shell.nix
@@ -11,14 +11,14 @@ pkgs.mkShellNoCC {
     editorconfig-checker
   ];
 
-  shellHook = ''
-    export OS_AUTH_URL=https://api.stack.it.ntnu.no:5000
-    export OS_PROJECT_ID=b78432a088954cdc850976db13cfd61c
-    export OS_PROJECT_NAME="STUDORG_Programvareverkstedet"
-    export OS_USER_DOMAIN_NAME="NTNU"
-    export OS_PROJECT_DOMAIN_ID="d3f99bcdaf974685ad0c74c2e5d259db"
-    export OS_REGION_NAME="NTNU-IT"
-    export OS_INTERFACE=public
-    export OS_IDENTITY_API_VERSION=3
-  '';
+  env = {
+    OS_AUTH_URL = "https://api.stack.it.ntnu.no:5000";
+    OS_PROJECT_ID = "b78432a088954cdc850976db13cfd61c";
+    OS_PROJECT_NAME = "STUDORG_Programvareverkstedet";
+    OS_USER_DOMAIN_NAME = "NTNU";
+    OS_PROJECT_DOMAIN_ID = "d3f99bcdaf974685ad0c74c2e5d259db";
+    OS_REGION_NAME = "NTNU-IT";
+    OS_INTERFACE = "public";
+    OS_IDENTITY_API_VERSION = "3";
+  };
 }
