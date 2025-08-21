f3c60d05512726929175aecb03c17ec050966291
Vegard Bieker Matthey <vegardbm@pvv.ntnu.no>
2025-08-21 14:21:39 +0200

    add vegardbm
    Reviewed-on: https://git.pvv.ntnu.no/Drift/pvv-nixos-config/pulls/114

---
+++ users/vegardbm.nix
@@ -0,0 +1,22 @@
+{ pkgs, ... }:
+{
+  users.users.vegardbm = {
+    isNormalUser = true;
+    description = "noe";
+    extraGroups = [
+      "wheel"
+      "drift"
+      "nix-builder-users"
+    ];
+    packages = with pkgs; [
+      btop
+      eza
+      neovim
+      ripgrep
+      tmux
+    ];
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDVA3HqEx3je6L1AC+bP8sTxu3ZTKvTCR0npCyOVAYK5 vbm@arch-xeon"
+    ];
+  };
+}
