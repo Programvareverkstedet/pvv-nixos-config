aa6f7e3038eb1a66c10bc9b0d4ad4333cd674e42
h7x4 <h7x4@nani.wtf>
2025-12-24 12:44:43 +0900

    base/nginx: enable extra listen paramenters by default

--- base/services/nginx.nix
+++ base/services/nginx.nix
@@ -40,6 +40,25 @@
   };
 
   services.nginx.virtualHosts."_" = lib.mkIf config.services.nginx.enable {
+    listen = [
+      {
+        addr = "0.0.0.0";
+        extraParameters = [
+          "default_server"
+          # Seemingly the default value of net.core.somaxconn
+          "backlog=4096"
+          "deferred"
+        ];
+      }
+      {
+        addr = "[::0]";
+        extraParameters = [
+          "default_server"
+          "backlog=4096"
+          "deferred"
+        ];
+      }
+    ];
     sslCertificate = "/etc/certs/nginx.crt";
     sslCertificateKey = "/etc/certs/nginx.key";
     addSSL = true;
