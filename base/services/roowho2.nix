4abe86dc077c244fba15db4f74587ff0aa00fb8b
h7x4 <h7x4@nani.wtf>
2026-01-06 11:31:41 +0900

    base/roowho2: traffic filter to PVV IP-space

--- base/services/roowho2.nix
+++ base/services/roowho2.nix
@@ -1,4 +1,12 @@
-{ ... }:
+{ lib, values, ... }:
 {
-  services.roowho2.enable = true;
+  services.roowho2.enable = lib.mkDefault true;
+
+  systemd.sockets.roowho2-rwhod.socketConfig = {
+    IPAddressDeny = "any";
+    IPAddressAllow = [
+      "127.0.0.1"
+      values.ipv4-space
+    ];
+  };
 }
