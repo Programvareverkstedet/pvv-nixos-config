9f43ea887e19ae4d5ff1b37f8314a0a28c058d5b
h7x4 <h7x4@nani.wtf>
2026-01-22 16:13:20 +0900

    base: OOM early on nixos rebuilds

--- base/nix.nix
+++ base/nix.nix
@@ -37,4 +37,9 @@
       "unstable=${inputs.nixpkgs-unstable}"
     ];
   };
+
+  # Make builds to be more likely killed than important services.
+  # 100 is the default for user slices and 500 is systemd-coredumpd@
+  # We rather want a build to be killed than our precious user sessions as builds can be easily restarted.
+  systemd.services.nix-daemon.serviceConfig.OOMScoreAdjust = lib.mkDefault 250;
 }
