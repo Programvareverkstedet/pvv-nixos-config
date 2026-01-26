1d57cec04d36cd0dbf0d0a99967ec3fae9d79ddd
h7x4 <h7x4@nani.wtf>
2026-01-26 15:07:40 +0900

    base/acme: remove deprecated argument

--- base/services/acme.nix
+++ base/services/acme.nix
@@ -8,8 +8,6 @@
   # Let's not spam LetsEncrypt in `nixos-rebuild build-vm` mode:
   virtualisation.vmVariant = {
     security.acme.defaults.server = "https://127.0.0.1";
-    security.acme.preliminarySelfsigned = true;
-
     users.users.root.initialPassword = "root";
   };
 }
