359f5996557d67938bedc624564d1b3da9878bdc
h7x4 <h7x4@nani.wtf>
2026-01-31 00:19:09 +0900

    bekkalokk/snappymail: add rsync pull target for principal

--- hosts/bekkalokk/services/webmail/snappymail.nix
+++ hosts/bekkalokk/services/webmail/snappymail.nix
@@ -14,5 +14,20 @@ in {
     enableACME = true;
     kTLS = true;
   };
-}
 
+  services.rsync-pull-targets = {
+    enable = true;
+    locations.${cfg.dataDir} = {
+      user = "root";
+      rrsyncArgs.ro = true;
+      authorizedKeysAttrs = [
+        "restrict"
+        "no-agent-forwarding"
+        "no-port-forwarding"
+        "no-pty"
+        "no-X11-forwarding"
+      ];
+      publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJENMnuNsHEeA91oX+cj7Qpex2defSXP/lxznxCAqV03 snappymail rsync backup";
+    };
+  };
+}
