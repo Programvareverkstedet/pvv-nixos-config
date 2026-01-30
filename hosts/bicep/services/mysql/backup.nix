cec69d89a85e6561dbde3ab370d48faaf4c7d9f2
h7x4 <h7x4@nani.wtf>
2026-01-30 13:26:10 +0900

    bicep/{postgres,mysql}: fix old backup deletion (again)

--- hosts/bicep/services/mysql/backup.nix
+++ hosts/bicep/services/mysql/backup.nix
@@ -56,8 +56,8 @@ in
       rm "$STATE_DIRECTORY/mysql-dump-latest.sql.zst" ||:
       ln -T "$OUT_FILE" "$STATE_DIRECTORY/mysql-dump-latest.sql.zst"
 
-      while [ $(find -type f -printf '.' "$STATE_DIRECTORY" | wc -c) -gt ${toString (rotations + 1)} ]; do
-        rm $(find "$STATE_DIRECTORY" -type f -printf '%T+ %p\n' | sort | head -n 1 | cut -d' ' -f2)
+      while [ "$(find "$STATE_DIRECTORY" -type f -printf '.' | wc -c)" -gt ${toString (rotations + 1)} ]; do
+        rm "$(find "$STATE_DIRECTORY" -type f -printf '%T+ %p\n' | sort | head -n 1 | cut -d' ' -f2)"
       done
     '';
 
