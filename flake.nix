f8dcaddefb79cc8a25ea9cf5844dff78ab4b1206
Vegard Bieker Matthey <VegardMatthey@protonmail.com>
2026-03-19 16:09:24 +0100

    use main branch for worblehat after merge

--- flake.nix
+++ flake.nix
@@ -23,7 +23,7 @@
     dibbler.url = "git+https://git.pvv.ntnu.no/Projects/dibbler.git?ref=main";
     dibbler.inputs.nixpkgs.follows = "nixpkgs";
 
-    worblehat.url = "git+https://git.pvv.ntnu.no/Projects/worblehat.git?ref=stable_deps";
+    worblehat.url = "git+https://git.pvv.ntnu.no/Projects/worblehat.git?ref=main";
     worblehat.inputs.nixpkgs.follows = "nixpkgs";
 
     matrix-next.url = "github:dali99/nixos-matrix-modules/v0.8.0";
