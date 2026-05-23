792f111a5d0c775bab718b00a1b08686f66880a7
Vegard Bieker Matthey <VegardMatthey@protonmail.com>
2026-05-23 23:36:13 +0200

    bekkalokk: pull rendered map from gluttony

--- hosts/bekkalokk/services/bluemap.nix
+++ hosts/bekkalokk/services/bluemap.nix
@@ -1,6 +1,7 @@
-{ config, lib, pkgs, inputs, ... }:
+{ config, lib, pkgs, inputs, values, ... }:
 let
   vanillaSurvival = "/var/lib/bluemap/vanilla_survival_world";
+  webExport = "/var/lib/bluemap/web";
   format = pkgs.formats.hocon { };
 in {
   # NOTE: our versino of the module gets added in flake.nix
@@ -115,6 +116,23 @@ in {
       quic_retry on;
       add_header Alt-Svc 'h3=":$server_port"; ma=86400';
     '';
+  };
+
+  services.rsync-pull-targets = {
+    enable = true;
+    locations.${webExport} = {
+      user = "root";
+      rrsyncArgs.wo = true;
+      authorizedKeysAttrs = [
+        "restrict"
+        "from=\"gluttony.pvv.ntnu.no,${values.hosts.gluttony.ipv6},${values.hosts.gluttony.ipv4}\""
+        "no-agent-forwarding"
+        "no-port-forwarding"
+        "no-pty"
+        "no-X11-forwarding"
+      ];
+      publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGYpjmQYs3pKV29/hVCeNQQ/WRmwGdhMEUJvAC99sZEp root@nixos";
+    };
   };
 
   networking.firewall.allowedUDPPorts = [ 443 ];
