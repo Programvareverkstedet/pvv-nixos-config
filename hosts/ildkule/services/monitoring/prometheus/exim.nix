3386153b8be02be56a6a011b65ece65d9ae9aff8
h7x4 <h7x4@nani.wtf>
2026-06-03 13:35:13 +0900

    ildkule/prometheus/exim: make scheme explicit

--- hosts/ildkule/services/monitoring/prometheus/exim.nix
+++ hosts/ildkule/services/monitoring/prometheus/exim.nix
@@ -1,14 +1,12 @@
 { ... }:
 {
-  services.prometheus = {
-    scrapeConfigs = [
-      {
-        job_name = "exim";
-        scrape_interval = "15s";
-        static_configs = [{
-          targets = [ "microbel.pvv.ntnu.no:9636" ];
-        }];
-      }
-    ];
-  };
+  services.prometheus.scrapeConfigs = [{
+    job_name = "exim";
+    scrape_interval = "15s";
+    scheme = "http";
+
+    static_configs = [{
+      targets = [ "microbel.pvv.ntnu.no:9636" ];
+    }];
+  }];
 }
