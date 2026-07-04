293f28abb102c5a14d81b2bef68fd5cbe0073821
h7x4 <h7x4@nani.wtf>
2026-07-05 01:01:35 +0900

    lupine5: set up bridge and tap netdevs

--- hosts/lupine/configuration.nix
+++ hosts/lupine/configuration.nix
@@ -15,14 +15,57 @@
     "i686-linux"
   ];
 
-  systemd.network.networks."30-enp0s31f6" = values.defaultNetworkConfig // {
-    matchConfig.Name = "enp0s31f6";
-    address = with values.hosts.${lupineName}; [ (ipv4 + "/25") (ipv6 + "/64") ];
-    networkConfig.LLDP = false;
-  };
-  systemd.network.wait-online = {
-    anyInterface = true;
-  };
+  systemd.network = if (lupineName != "lupine-5")
+    then {
+      networks."30-enp0s31f6" =  (values.defaultNetworkConfig // {
+        matchConfig.Name = "enp0s31f6";
+        address = with values.hosts.${lupineName}; [ (ipv4 + "/25") (ipv6 + "/64") ];
+        networkConfig.LLDP = false;
+      });
+
+      wait-online = {
+        anyInterface = true;
+      };
+    }
+    else {
+      netdevs."10-br0".netdevConfig = {
+        Name = "br0";
+        Kind = "bridge";
+      };
+
+      netdevs."20-tap0".netdevConfig = {
+        Name = "tap0";
+        Kind = "tap";
+      };
+
+      networks."10-enp0s31f6" = {
+        matchConfig.Name = "enp0s31f6";
+        bridge = [ "br0" ];
+      };
+
+      networks."20-br0" = {
+        matchConfig.Name = "br0";
+
+        address = with values.hosts.${lupineName}; [ (ipv4 + "/25") (ipv6 + "/64") ];
+        networkConfig.LLDP = false;
+
+        dns = ["129.241.0.200" "129.241.0.201" "2001:700:300:1900::200" "2001:700:300:1900::201"];
+        domains = ["pvv.ntnu.no" "pvv.org"];
+        gateway = [values.hosts.gateway values.hosts.gateway6];
+
+        networkConfig.IPv6AcceptRA = "no";
+        DHCP = "no";
+      };
+
+      networks."30-tap0" = {
+        matchConfig.Name = "tap0";
+        bridge = [ "br0" ];
+      };
+
+      wait-online = {
+        anyInterface = true;
+      };
+    };
 
   # Don't change (even during upgrades) unless you know what you are doing.
   # See https://search.nixos.org/options?show=system.stateVersion
