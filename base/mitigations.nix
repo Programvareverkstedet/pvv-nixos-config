e2567e78cfd5698eeab124db0f683cc6b223faa6
Daniel Olsen <daniel.olsen99@gmail.com>
2026-07-28 17:36:31 +0200

    mitigations: patch matrix-synapse

--- base/mitigations.nix
+++ base/mitigations.nix
@@ -21,4 +21,18 @@ in
   boot.blacklistedKernelModules = modulesToBan;
 
   boot.extraModprobeConfig = lib.concatMapStringsSep "\n" (mod: "install ${mod} ${lib.getExe' pkgs.coreutils "false"}") modulesToBan;
+
+  nixpkgs.overlays = [
+    (final: prev: {
+      matrix-synapse-unwrapped = prev.matrix-synapse-unwrapped.overrideAttrs (old: rec {
+        version = "1.157.2";
+        src = final.fetchFromGitHub {
+          owner = "element-hq";
+          repo = "synapse";
+          rev = "v${version}";
+          hash = "sha256-TUHcNAXrV43+J7jqfstlYdrZrwL5kDCh03yxO+vL/gw=";
+        };
+      });
+    })
+  ];
 }
