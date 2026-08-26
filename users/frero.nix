2f0605c32fe884027826f429ed260f7022da6059
Fredrik Robertsen <fredrikr79@pm.me>
2026-08-26 09:53:12 +0200

    frero: add new laptop key

--- users/frero.nix
+++ users/frero.nix
@@ -5,7 +5,7 @@
     extraGroups = [ "wheel" "drift" "nix-builder-users" ];
     shell = if config.programs.zsh.enable then pkgs.zsh else pkgs.bash;
     openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII09JbtSUMurvmHpJ7TmUQctXpNVhjFYhoJ3+1ZITmMx"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKULJg19S/Ny9bbIH48OXa1iNsXuyr8e3Ym7mJCIgbnX" # laptop
     ];
   };
 }
