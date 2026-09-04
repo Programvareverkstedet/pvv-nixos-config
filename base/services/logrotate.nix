da98ee5ad4e7b7eeeab5b60fab3d46e305b9d253
h7x4 <h7x4@nani.wtf>
2026-09-04 19:57:26 +0900

    base/logrotate: use zst

--- base/services/logrotate.nix
+++ base/services/logrotate.nix
@@ -1,5 +1,13 @@
-{ ... }:
+{ pkgs, ... }:
 {
+  services.logrotate.settings.header = {
+    compress = true;
+    compresscmd = "${pkgs.zstd}/bin/zstd";
+    compressoptions = "-T0 --rm -q";
+    uncompresscmd = "${pkgs.zstd}/bin/unzstd";
+    compressext = ".zst";
+  };
+
   systemd.services.logrotate = {
     documentation = [ "man:logrotate(8)" "man:logrotate.conf(5)" ];
     unitConfig.RequiresMountsFor = "/var/log";
