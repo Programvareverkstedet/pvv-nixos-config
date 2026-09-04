d4caa3ae3f6b33f734d82f657b2b13031b48a653
h7x4 <h7x4@nani.wtf>
2026-09-04 17:51:47 +0900

    kommode/duperemove: init, dedupe gitea-web

---
+++ hosts/kommode/services/duperemove.nix
@@ -0,0 +1,41 @@
+{ lib, pkgs, ... }:
+{
+  systemd.services.duperemove-gitea-web = {
+    description = "Deduplicate /var/lib/gitea-web contents";
+    startAt = "weekly";
+
+    confinement.enable = true;
+
+    serviceConfig = {
+      Type = "oneshot";
+      CacheDirectory = "duperemove";
+      DynamicUser = true;
+
+      ExecStart =
+        let
+          args = lib.cli.toCommandLineShellGNU { } {
+            d = true;
+            r = true;
+            hashfile = "%C/duperemove/gitea-web.hash";
+            "io-threads" = 2;
+            "cpu-threads" = 2;
+            b = "4k";
+            "dedupe-options" = "partial";
+            "batchsize" = 128;
+          };
+        in
+        "${lib.getExe pkgs.duperemove} ${args} /var/lib/gitea-web";
+
+      BindPaths = [ "/var/lib/gitea-web" ];
+
+      Nice = 19;
+      CPUSchedulingPolicy = "idle";
+      IOSchedulingClass = "idle";
+
+      PrivateNetwork = true;
+      CapabilityBoundingSet = [ "CAP_DAC_OVERRIDE" ];
+      AmbientCapabilities = [ "CAP_DAC_OVERRIDE" ];
+      PrivateUsers = lib.mkForce false;
+    };
+  };
+}
