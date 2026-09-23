236231a4e3c9b11723fe8ecc2817efc1c2afffc8
h7x4 <h7x4@nani.wtf>
2026-09-24 03:27:45 +0900

    base/prometheus-node-exporter: use systemd unix socket

--- base/services/prometheus-node-exporter.nix
+++ base/services/prometheus-node-exporter.nix
@@ -1,6 +1,7 @@
-{ config, lib, values, ... }:
+{ config, lib, pkgs, values, ... }:
 let
   cfg = config.services.prometheus.exporters.node;
+  socketPath = "/run/prometheus-node-exporter.sock";
 in
 {
   services.prometheus.exporters.node = {
@@ -8,6 +9,31 @@ in
     listenAddress = "127.0.0.1";
     port = 9100;
     enabledCollectors = [ "systemd" ];
+  };
+
+  # https://github.com/NixOS/nixpkgs/issues/510379
+  systemd.sockets.prometheus-node-exporter = lib.mkIf cfg.enable {
+    wantedBy = [ "sockets.target" ];
+    socketConfig = {
+      ListenStream = socketPath;
+      SocketGroup = config.services.nginx.group;
+      SocketMode = "0660";
+    };
+  };
+
+  systemd.services = lib.mkIf cfg.enable {
+    "prometheus-node-exporter" = {
+      serviceConfig = {
+        Slice = "system-monitoring.slice";
+        ExecStart = let
+          args = lib.cli.toCommandLineShellGNU { } (
+               (lib.listToAttrs (map (x: lib.nameValuePair "collector.${x}" true) cfg.enabledCollectors))
+            // (lib.listToAttrs (map (x: lib.nameValuePair "no-collector.${x}" true) cfg.disabledCollectors))
+            // { "web.systemd-socket" = true; }
+          );
+        in lib.mkForce "${pkgs.prometheus-node-exporter}/bin/node_exporter ${args} ${lib.escapeShellArgs cfg.extraFlags}";
+      };
+    };
   };
 
   services.nginx = lib.mkIf cfg.enable {
@@ -19,7 +45,7 @@ in
       kTLS = true;
 
       locations."/prometheus-node-exporter/metrics" = {
-        proxyPass = "http://localhost:${toString cfg.port}/metrics";
+        proxyPass = "http://unix:${socketPath}:/metrics";
 
         extraConfig = ''
           allow 127.0.0.1;
@@ -30,9 +56,5 @@ in
         '';
       };
     };
-  };
-
-  systemd.services = lib.mkIf cfg.enable {
-    "prometheus-node-exporter".serviceConfig.Slice = "system-monitoring.slice";
   };
 }
